12b0422c19693c55b9442e380bc005b978414690
susondra <123081753+susondra@users.noreply.github.com>
2023-02-08 18:15:38 +0100

    Aktualizovat pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,20 @@
+input.calibrateCompass()
 basic.forever(function () {
-    basic.showIcon(IconNames.Heart)
-    basic.showIcon(IconNames.SmallHeart)
+    if (input.compassHeading() >= 337 || input.compassHeading() <= 23) {
+        basic.showArrow(ArrowNames.North)
+    } else if (input.compassHeading() > 23 && input.compassHeading() < 67) {
+        basic.showArrow(ArrowNames.NorthEast)
+    } else if (input.compassHeading() >= 67 && input.compassHeading() <= 113) {
+        basic.showArrow(ArrowNames.East)
+    } else if (input.compassHeading() > 113 && input.compassHeading() < 157) {
+        basic.showArrow(ArrowNames.SouthEast)
+    } else if (input.compassHeading() >= 157 && input.compassHeading() <= 203) {
+        basic.showArrow(ArrowNames.South)
+    } else if (input.compassHeading() > 203 && input.compassHeading() < 247) {
+        basic.showArrow(ArrowNames.SouthWest)
+    } else if (input.compassHeading() >= 247 && input.compassHeading() <= 293) {
+        basic.showArrow(ArrowNames.West)
+    } else {
+        basic.showArrow(ArrowNames.NorthWest)
+    }
 })
